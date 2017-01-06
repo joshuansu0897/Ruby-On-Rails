ac386965359852ace55cbd9e85633d96e7ec3efa
joshuansu0897 <joshuansu0897@gmail.com>
2017-01-05 23:11:52 -0700

    se modificaron los archivos de los comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :comments
+  resources :articles do
+     resources :comments, only: [:create, :destroy, :update]
+  end
+ 
   devise_for :users
-  resources :articles
-
+ 
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
