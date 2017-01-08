5727e8bb5bde120b5c1c59b595b2f0a0eae95e2d
joshuansu0897 <joshuansu0897@gmail.com>
2017-01-08 14:45:45 -0700

    se colo todo para subir imagenes

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105233800) do
+ActiveRecord::Schema.define(version: 20170108204651) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.integer  "visits_count"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "cover_file_name"
+    t.string   "cover_content_type"
+    t.integer  "cover_file_size"
+    t.datetime "cover_updated_at"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
